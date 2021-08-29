e6392ac38c10c265647de956cb637913dd482329
Victor Fabrício <victorfcp@hotmail.com>
2021-08-29 11:08:03 -0300

    Update LeadsList.tsx

--- store-block-template/react/LeadsList.tsx
+++ store-block-template/react/LeadsList.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 interface LeadsListProps {}
-const lista = "LISTA DE LEADS"
 
 const LeadsList: StorefrontFunctionComponent<LeadsListProps> = ({}) => {
   return (
    <div>
        <br/>
        <br/>
-     <li>{lista}</li>
+     <h1>      LISTA DE LEADS</h1>
     </div>
   )
 }
