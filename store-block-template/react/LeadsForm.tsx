f9f01e6a3b0074cd7573d7e060fe332105732b4a
joirneto <joirneto@gmail.com>
2021-08-29 11:05:32 -0300

    Leads page

--- store-block-template/react/LeadsForm.tsx
+++ store-block-template/react/LeadsForm.tsx
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-interface LeadsFormProps {}
+interface LeadsFormProps { }
 
-const LeadsForm: StorefrontFunctionComponent<LeadsFormProps> = ({}) => {
+const LeadsForm: StorefrontFunctionComponent<LeadsFormProps> = ({ }) => {
+  const [email, setEmail] = useState('');
+  const [erro, setErro] = useState(false);
+
+  function handlePesquisa() {
+    axios.get(`https://joirdev--hiringcoders202111.myvtex.com/_v/leads`)
+    .then(response => {
+      const repositories = response.data;
+    //  const repositoriesName = [];
+    console.log("TESTE1= ")
+    console.log("TESTE= ", repositories)
+
+      // eslint-disable-next-line array-callback-return
+   //   repositories.map((repository) => {
+   //     repositoriesName.push(repository.name)
+  //    });
+
+   //   localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setErro(false);
+
+    })
+    .catch(err => {
+      setErro(true);
+    });
+  }
+  
   return (
-   <div>
-     <form>
-      Nome: <br/>
-     <input type="text" name="name" /> <br/>
-     Telefone: <br/>
-     <input type="text" name="telephone"/> <br/>
-     Email: <br/>
-     <input type="text" name="email"/> <br/>
-     <br/>
-     <button>Cadastrar na Newsletter</button>
-     </form>
-     </div>
+    <div>
+      <form onSubmit={e => e.preventDefault()}>
+        Nome: <br />
+        <input type="text" name="name" /> <br />
+        Telefone: <br />
+        <input type="text" name="telephone" /> <br />
+        Email: <br />
+        <input type="email"  name="email" value = {email} onChange = {e => setEmail((e.target as HTMLInputElement).value)}/> <br />
+        <br />
+        <button onClick={handlePesquisa}>Cadastrar na Newsletter</button>
+        {erro ? <span>Dados não encontrados.</span> : ''}
+      </form>
+    </div>
   )
 }
 
--- store-block-template/react/LeadsList.tsx
+++ store-block-template/react/LeadsList.tsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-interface LeadsListProps {}
-const lista = "LISTA DE LEADS"
+interface LeadsListProps { }
 
-const LeadsList: StorefrontFunctionComponent<LeadsListProps> = ({}) => {
+const LeadsList: StorefrontFunctionComponent<LeadsListProps> = ({ }) => {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://joirdev--hiringcoders202111.myvtex.com/_v/leads`)
+      .then(response => {
+        setRepositories(response.data.Items)
+        const test = response.data.Items
+
+      
+
+        
+
+       
+
+      //  repositoriesEmail = JSON.parse(repositoriesEmail);
+      //  setRepositories(repositoriesEmail);
+
+        
+
+      });
+
+
+
+
+  }, []);
+
+  console.log("TESTE3= ", repositories)
+
+
+
+
   return (
-   <div>
-       <br/>
-       <br/>
-     <li>{lista}</li>
+
+    <div>
+      <br />
+      <br />
+      <h1>LEADS</h1>
+      <>
+      <ul>
+       {repositories.map(repository =>{
+         return(
+           <li>Nome: {repository.name}  - Email: {repository.email} - Phone: {repository.phone}</li>
+         )
+       })
+
+       } 
+      </ul>
+      </>
+
+
+
     </div>
   )
 }
