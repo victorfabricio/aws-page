70675cf7a3c04c92533763c33af26486712cdb41
JKAmadio <71294182+JKAmadio@users.noreply.github.com>
2021-08-30 16:52:08 -0300

    Correção nas informações dos clientes pós venda

--- ap-vtex-aws/node/middlewares/someStates.ts
+++ ap-vtex-aws/node/middlewares/someStates.ts
@@ -6,12 +6,14 @@ export async function someStates(
 ) {
   const orderId = ctx.body.orderId
   const orderBody = await ctx.clients.order.order(orderId)
-  console.log('ORDER_ID ==', orderId)
-  console.log('ORDER_BODY ==', orderBody)
 
   const profileId = orderBody.clientProfileData.userProfileId
+  const userFirstName = (await ctx.clients.profile.getEmail(profileId))
+    .firstName
+  const userLastName = (await ctx.clients.profile.getEmail(profileId)).lastName
+  const userFullName = userFirstName + ' ' + userLastName
+  const userPhone = (await ctx.clients.profile.getEmail(profileId)).homePhone
   const userEmail = (await ctx.clients.profile.getEmail(profileId)).email
-  console.log('USER EMAIL ==', userEmail)
 
   axios
     .get(
@@ -24,37 +26,37 @@ export async function someStates(
     )
     .then((response) => {
       if (typeof response.data.Item === 'undefined') {
-        console.info('POST')
         axios
           .post(`http://jkamadio--hiringcoders202111.myvtex.com/_v/leads/new`, {
             id: userEmail,
             email: userEmail,
-            name: userEmail,
-            phone: userEmail,
+            name: userFullName,
+            phone: userPhone,
             category: 'cliente',
           })
           .then((response) => {
             console.log(response)
-            setErro(false)
           })
           .catch((err) => {
-            setErro(true)
+            console.info(err)
           })
       } else {
-        console.info('PATCH')
         axios
           .patch(
             `https://50ucek28u1.execute-api.sa-east-1.amazonaws.com/leads/${userEmail}`,
             {
               headers: {
-                'Cache-Control': 'no-cache, no-store, must-revalidate', 'X-VTEX-Use-Https': 'true', 'Proxy-Authorization': 'ctx.authToken',
-              }
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                'X-VTEX-Use-Https': 'true',
+                'Proxy-Authorization': 'ctx.authToken',
+              },
+            }
           )
           .then((response) => {
             console.log(response)
           })
           .catch((err) => {
-            console.info('ERROR 2 =', err)
+            console.info(err)
           })
       }
     })
